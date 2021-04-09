b341ac6fa55391a705e0147fe81efb989087dd6f
sjjeong <sjjeong1225@gmail.com>
2021-04-10 02:29:17 +0900

    파일 이동

--- MVVMRxSwift/ui/restaurant/RestaurantViewController.swift
+++ MVVMRxSwift/ui/restaurant/RestaurantViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RestaurantViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ MVVMRxSwift/ui/restaurant/RestaurantViewModel.swift
@@ -0,0 +1,8 @@
+//
+// Created by SeokJun Jeong on 2021/04/10.
+//
+
+import Foundation
+
+class RestaurantViewModel {
+}
