599bcf793e26cda134d87a60a7b02aaca008afa3
sjjeong <sjjeong1225@gmail.com>
2021-04-13 17:49:18 +0900

    AppCoordinator 추가

---
+++ MVVMRxSwift/AppCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  AppCoordinator.swift
+//  MVVMRxSwift
+//
+//  Created by SeokJun Jeong on 2021/04/13.
+//
+
+import UIKit
+
+
+class AppCoordinator {
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        
+    }
+}
